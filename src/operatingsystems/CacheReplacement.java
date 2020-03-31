2a569db6c93acdd439cfc41d7bb75e642efece29
sharondsza26 <sharondsouza26@gmail.com>
2020-04-01 02:34:04 +0530

    Least Recently Used(LRU) Cache Replacement Policy

--- src/operatingsystems/CacheReplacement.java
+++ src/operatingsystems/CacheReplacement.java
@@ -8,11 +8,11 @@ import java.util.Scanner;
 public class CacheReplacement {
 
     public static void main(String args[]) {
-        int m = 1;
+        int id = 1;
         Scanner sc = new Scanner(System.in);
-        HashMap<Integer, String> songMap = new HashMap<Integer, String>();
-        HashMap<Integer, Integer> cacheMap = new HashMap<Integer, Integer>();
-
+        HashMap<Integer, String> songMap = new HashMap<>();
+        HashMap<Integer, Integer> cacheMap = new HashMap<>();
+        LinkedList<Integer> listCache = new LinkedList<>();
         Queue<Integer> fifoCache = new LinkedList<>();
         Stack<Integer> lifoCache = new Stack<>();
         populate(songMap);
@@ -20,12 +20,13 @@ public class CacheReplacement {
         //User input
         do {
             System.out.println("Select songs you want to play");
-            m = sc.nextInt();
-            updateLFU(cacheMap, m);
-            updateFifo(fifoCache, m);
-            updateLifo(lifoCache, m);
+            id = sc.nextInt();
+//            updateFifo(fifoCache, id);
+//            updateLifo(lifoCache, id);
+//            updateLFU(cacheMap, id);
+            updateLRU(listCache, id);
             System.out.println("Enter -1 to exit");
-        } while (m != -1);
+        } while (id != -1);
         display(fifoCache, lifoCache);
 
     }
@@ -47,14 +48,14 @@ public class CacheReplacement {
         songMap.forEach((k, v) -> System.out.println(k + "  :  " + v));
     }
 
-    public static void updateFifo(Queue fifoCache, int m) {
+    public static void updateFifo(Queue fifoCache, int id) {
 
         //Initially
         System.out.println("FIFO CACHE : " + fifoCache);
 
         //Adding songs to Cache
-        boolean song = (boolean) fifoCache.contains(m);
-        fifoCache.add(m);
+        boolean song = (boolean) fifoCache.contains(id);
+        fifoCache.add(id);
         System.out.println("FIFO CACHE : " + fifoCache);
 
         //Updating Cache
@@ -67,13 +68,13 @@ public class CacheReplacement {
         }
     }
 
-    public static void updateLifo(Stack lifoCache, int m) {
+    public static void updateLifo(Stack lifoCache, int id) {
         //initially
         System.out.println("LIFO CACHE :" + lifoCache);
 
         //Adding songs to Cache
-        Integer song = (Integer) lifoCache.search(m);
-        lifoCache.push(m);
+        Integer song = (Integer) lifoCache.search(id);
+        lifoCache.push(id);
         System.out.println("LIFO CACHE : " + lifoCache);
 
         //Updating Cache
@@ -88,30 +89,62 @@ public class CacheReplacement {
 
     public static void updateLFU(HashMap<Integer, Integer> cacheMap, int id) {
 
+        //Updating cache if song present
         if (cacheMap.containsKey(id)) {
             int newVal = cacheMap.get(id) + 1;
             cacheMap.put(id, newVal);
         } else {
-
+            
+            //Updating Cache after it's full
             if (cacheMap.size() == 5) {
                 int minVal = Integer.MAX_VALUE;
                 int keyToRemove = 0;
                 for (int key : cacheMap.keySet()) {
                     int value = cacheMap.get(key);
-                    if(value <= minVal){
+                    if (value <= minVal) {
                         minVal = value;
                         keyToRemove = key;
                     }
-                    
+
                 }
                 cacheMap.remove(keyToRemove);
                 cacheMap.put(id, 1);
 
             } else {
+                //adding song to cache
                 cacheMap.put(id, 1);
             }
         }
         System.out.println("Map: " + cacheMap);
+    }
+    
+    public static void updateLRU(LinkedList<Integer> listCache, int id){
+        
+        //adding songs to Empty Cache
+        if(listCache.isEmpty()){
+            listCache.add(id);
+        } else {
+            
+            //Updating cache if song present
+            if(listCache.contains(id)){
+                int temp = id;
+                listCache.remove(new Integer(id));
+                listCache.addFirst(temp);
+            } else{
+                
+                //Updating cache after it's full
+                if (!listCache.contains(id) && listCache.size() == 5){
+                    listCache.removeLast();
+                    listCache.addFirst(id);
+                } else{
+                    
+                    //adding to Cache
+                    listCache.addFirst(id);
+                }
+            }
+        }
+        System.out.println("List: "+ listCache);
+        
     }
 
 //Display Table
